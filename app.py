1813d3c3738f7fa954337c329ab232c7e8f185ee
Cade Jackson <cade@depthofcut.com>
2021-06-21 01:21:27 -0500

    Update notebooks

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-# Import Flask
-from flask import Flask
+# Import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
-# Create a new flask app instance
+# Create engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Reflect the database
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+# Save table references
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create session
+session = Session(engine)
+
+# Set up Flask
 app = Flask(__name__)
 
-# Create Flask Routes
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+# Create routes
+@app.route("/")
+
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
